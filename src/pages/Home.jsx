1f969decb54804b7b1047e130f7e99f5007bdb79
Haris Rajput <harisahmed321@gmail.com>
2024-02-23 18:53:35 +0500

    mine changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome to home</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam officiis illo perferendis voluptatem laborum sunt veritatis eaque saepe
         quos. Officia soluta consequatur explicabo esse atque, recusandae expedita et officiis aliquid Lorem ipsum dolor sit amet consectetur
@@ -12,7 +12,7 @@ export default function Home() {
         quos. Officia soluta consequatur explicabo esse atque, recusandae expedita et officiis aliquid Lorem, ipsum dolor sit amet consectetur
         adipisicing elit. Debitis facilis commodi quos ducimus accusantium dolore atque nostrum voluptatum exercitationem possimus!
       </p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, ipsa.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, ipsa. This is mine text</p>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam officiis illo perferendis voluptatem laborum sunt veritatis eaque saepe
         quos. Officia soluta consequatur explicabo esse atque, recusandae expedita et officiis aliquid Lorem ipsum, dolor sit amet consectetur
